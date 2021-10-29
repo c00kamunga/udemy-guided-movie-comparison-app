07018d5475178bc8f9c3670782641c56d4351c32
Barinder <tsothalanti2@yahoo.com>
2021-10-28 20:09:57 -0700

    added timeout function for when user types in a movie to the input field

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '35782786',
-      i: 'tt0848228',
+      s: searchTerm,
     },
   });
   console.log(response.data);
 };
+const input = document.querySelector('input');
 
-fetchData();
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+
+input.addEventListener('input', onInput);
