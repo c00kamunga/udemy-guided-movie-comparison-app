56426e6dea720d9f4ecee6702c94990cb47c2e57
Barinder <tsothalanti2@yahoo.com>
2021-10-28 19:21:27 -0700

    added fetchData function for retrieving data from API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '35782786',
+      i: 'tt0848228',
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();
