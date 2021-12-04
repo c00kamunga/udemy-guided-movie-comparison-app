6f15940f48036beebad295fe45fe012b644bc7de
Barinder <tsothalanti2@yahoo.com>
2021-12-03 19:54:34 -0800

    added code to allow user to click outside of search result to close current results

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const onInput = async (event) => {
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
     option.classList.add('dropdown-item');
     option.innerHTML = `
@@ -46,3 +46,9 @@ const onInput = async (event) => {
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
