2f77ca0d73681e453d97b5d8b0832bd4c62d89cc
Barinder <tsothalanti2@yahoo.com>
2021-12-05 00:47:13 -0800

    clicking on an option by user for left or right side will now render option summary to respective side

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const autoCompleteConfig = {
     ${movie.Title} (${movie.Year})
     `;
     },
-    onOptionSelect(movie) {
-        document.querySelector('.tutorial').classList.add('is-hidden')
-        onMovieSelect(movie);
-    },
+
     inputValue(movie) {
         return movie.Title;
     },
@@ -32,13 +29,21 @@ const autoCompleteConfig = {
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#left-summary'));
+    },
 });
 createAutoComplete({
     ...autoCompleteConfig,
     root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('#right-summary'));
+    },
 });
 
-const onMovieSelect = async(movie) => {
+const onMovieSelect = async(movie, summaryElement) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '35782786',
@@ -46,7 +51,7 @@ const onMovieSelect = async(movie) => {
         },
     });
 
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+    summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
