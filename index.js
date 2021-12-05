0a53d8d72feaad08c0d56618708b20dddb72d2cf
Barinder <tsothalanti2@yahoo.com>
2021-12-05 00:12:38 -0800

    fully refactored autocomplete function to be fully reusable for other applications

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const fetchData = async(searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '35782786',
-            s: searchTerm,
-        },
-    });
-    if (response.data.Error) {
-        return [];
-    }
-
-    return response.data.Search;
-};
-
 createAutoComplete({
     root: document.querySelector('.autocomplete'),
     renderOption(movie) {
@@ -22,11 +8,25 @@ createAutoComplete({
         `;
     },
     onOptionSelect(movie) {
-        onMovieSelect(movie)
+        onMovieSelect(movie);
     },
     inputValue(movie) {
-        return movie.Title
-    }
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: '35782786',
+                s: searchTerm,
+            },
+        });
+
+        if (response.data.Error) {
+            return [];
+        }
+
+        return response.data.Search;
+    },
 });
 
 const onMovieSelect = async(movie) => {
